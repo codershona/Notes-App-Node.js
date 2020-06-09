cec892e002fb188940a56273c111445cac8fc3fa
codershona <falgunimst95@gmail.com>
2020-06-09 17:13:35 +0200

    Import Your own files

--- app.js
+++ app.js
@@ -1,7 +1,50 @@
 // console.log('Wellcome to the class!')
 
-const fs = require('fs')
+// const fs = require('fs')
 
-fs.writeFileSync('note.text', 'this file is created by nodejs language.')
+// const fileSystem = require('fs')
+
+// fs.writeFileSync('note.text', 'This is node.js, this file is created by nodejs language.')
+
+// fileSystem.writeFileSync('note.text', 'This is node.js.')
+
+ //fs.appendFileSync('note.txt', ' I live in Canada.')
+
+// Challenge: Append a message to notes.txt
+
+// Use appendFileSync to append to the file
+// Run the script
+// Check your work by opening the file and viewing the appended text
+
+// require('./utils.js')
+
+
+// const name = 'John'
+
+// const name = require('./utils.js')
+// const firstName = require('./utils.js')
+// const add = require('./utils.js')
+
+// // console.log(firstName)
+// const sum = add(4, -2)
+
+
+// console.log(sum)
+
+// Challenge: Define and use a function in a new file
+
+// Create a new file called note.js
+
+// Create get n
+
+
+const getNotes = require('./notes.js')
+
+const msg = getNotes()
+
+console.log(msg)
+
+
+
 
 
---
+++ utils.js
@@ -0,0 +1,14 @@
+console.log('utils.js')
+
+
+const name = 'Hike'
+
+const add = function (a, b) {
+  return a + b
+
+}
+
+// module.exports = name
+module.exports = add
+
+
