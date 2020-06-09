95a4a5a6a8159046c135276d218b3f63a1feb6c7
codershona <falgunimst95@gmail.com>
2020-06-09 16:34:14 +0200

    Importing Node.js Core modules : part 1

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Notes-App-Node.js
 Notes App Node.js
+
+
+* NOTES :
+
+
+  ```
+    - Run node app.js ;
+    
+
+  ```
---
+++ app.js
@@ -0,0 +1,7 @@
+// console.log('Wellcome to the class!')
+
+const fs = require('fs')
+
+fs.writeFileSync('note.text', 'this file is created by nodejs language.')
+
+
