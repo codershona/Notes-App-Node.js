301d8a7cf72e6f2d7a5eebd40ffad9a3f1eb5642
codershona <falgunimst95@gmail.com>
2020-06-21 13:22:05 +0200

    The CallBack Function done

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,1 +1,67 @@
-setTimeout()
+// setTimeout(() => {
+// 	console.log('Two seconds are up')
+// }, 2000)
+
+// const names = ['Rita', 'Amena', "Kajol"]
+
+// const shortNames = names.filter((name) => {
+// 	return name.length <= 4
+
+// })
+
+
+// const geocode = (address, callback) => {
+//    // const data = {
+//    // 	latitude: 0,
+//    // 	longitude: 0
+
+//    // }
+ 
+//    // return data
+
+//    setTimeout(() => {
+
+//    const data = {
+//    	 latitude: 0,
+//    	 longitude: 0
+
+//    }
+
+//    callback(data)
+ 
+//    // return data
+
+
+//    }, 2000)
+
+// }
+
+
+// // geocode('Philadelphia', () => {
+
+// // })
+
+// geocode('Philadelphia', (data) => {
+// 	console.log(data)
+
+// })
+
+// // const data =  geocode('Philadelphia')
+
+// // console.log(data)
+
+// // RUN in terminal : node 4-callbacks.js
+
+const add = (a, b, callback) => {
+	setTimeout(() => {
+		callback(a + b)
+
+	}, 2000)
+
+  
+
+}
+
+add(1, 4, (sum) => {
+	console.log(sum)  // Should print: 5
+})
--- playground/Readme.md
+++ playground/Readme.md
@@ -8,5 +8,7 @@
    - node 1-json.js ;
    - nodemon 2-arrow-function.js ;
    - nodemon 3-arrow-challenge.js ;
+   - RUN node 4-callbacks.js ;
+   - 
 
 ```
